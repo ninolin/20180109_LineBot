e8e9b30fc78e86bec75ad49570e1df4438ffa7d1
ninolin <a7872278722@gmail.com>
2019-02-13 23:41:47 +0800

    test

--- step10_vision.php
+++ step10_vision.php
@@ -47,6 +47,7 @@
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
     curl_setopt($ch, CURLOPT_HTTPHEADER, $header);                                                                                                   
     $result = json_decode(curl_exec($ch));
+    fwrite($myfile, "\xEF\xBB\xBF".json_encode($result));
     $result_ary = mb_split("\n",$result -> responses[0] -> fullTextAnnotation -> text);
     $ans_txt = "這張發票沒用了，你又製造了一張垃圾";
     foreach ($result_ary as $val) {

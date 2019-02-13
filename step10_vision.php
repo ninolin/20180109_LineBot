4fa24fb29a06b817b36f7eb281ecf6120c9a3067
ninolin <a7872278722@gmail.com>
2019-02-13 23:52:41 +0800

    test

--- step10_vision.php
+++ step10_vision.php
@@ -49,9 +49,10 @@
     $result = json_decode(curl_exec($ch));
     
     $result_ary = mb_split("\n",$result -> responses[0] -> fullTextAnnotation -> text);
-    fwrite($myfile, "\xEF\xBB\xBF".json_encode($result -> responses[0]));
+    fwrite($myfile, "cccc");
     $ans_txt = "這張發票沒用了，你又製造了一張垃圾";
     foreach ($result_ary as $val) {
+        fwrite($myfile, $val);
         if($val == "MB-76164441"){
           $ans_txt = "恭喜您中獎啦，快分紅!!";
         }
